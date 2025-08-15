d8542b16c5b44b8fa29a357cc0b6cc68140d0e3f
pujeetas <pujeeta98@gmail.com>
2025-08-15 22:00:24 +0530

    added code for progress bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [percent, setPercent] = useState(0);
 
+  function handleButton(num) {
+    const newPercent = percent + num;
+    if (newPercent < 0 || newPercent > 100) {
+      return;
+    }
+    setPercent(newPercent);
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1 className="container-heading">Progress Bar</h1>
+      <div className="bar">
+        <div
+          className="progress-fill"
+          style={{
+            width: `${percent}%`,
+            backgroundColor:
+              percent > 80 ? "green" : percent < 40 ? "red" : "orange",
+          }}
+        ></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="progress-btn-container">
+        <button onClick={() => handleButton(+10)}>+10%</button>
+        <button onClick={() => handleButton(-10)}>-10%</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
